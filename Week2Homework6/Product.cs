f40559a5ceb8b692cd5404fc51e7e4a3a1681a1c
YASİN IRBIK <irbikyasin@gmail.com>
2021-02-25 14:02:41 +0300

    25.02.2021 genel duzenleme

---
+++ Week2Homework6/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week2Homework6
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public int Fiyat { get; set; }
+        public string Kategori { get; set; }
+    }
+}
